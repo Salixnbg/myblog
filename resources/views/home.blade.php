c45ea6261a63b2c0b5f4c709bddb3670ffa09b72
Salix NIBIGIRA <salixnibigira@gmail.com>
2023-06-15 14:55:18 +0200

    bootstrap_bootswatch_frontawesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('base')
 
 @section('content')
-    <div class="jumbotron">
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
         <h1 class="display-4">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr class="my-4">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-primary btn-lg" href="#" role="button">
+        Learn more
+        <i class="fa-solid fa-arrow-right"></i>
+        </a>
     </div>
-    
 @endsection
